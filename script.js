d981be720e81b283b1e80d9a9c85f13225b7f91f
Gustavo Sthel <gustavosthel@gmail.com>
2024-04-12 20:30:40 -0300

    concluded responsive + javascript

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const sidebarItems = document.querySelectorAll('.sidebar .item');
 const tableRows = document.querySelectorAll('.main table tbody tr');
 
+const menuBtn = document.getElementById('menu-btn');
+const leftSection = document.querySelector('.left-section');
+
+let isMenuOpen = false;
+
 sidebarItems.forEach(sideItem => {
     sideItem.addEventListener('click', () => {
         sidebarItems.forEach(item => {
@@ -19,3 +24,11 @@ tableRows.forEach(tableTr => {
     });
 });
 
+menuBtn.addEventListener('click', () => {
+    if (!isMenuOpen){
+        leftSection.style.left = '0';
+    } else {
+        leftSection.style.left = '-160px';
+    }
+    isMenuOpen = !isMenuOpen;
+});
